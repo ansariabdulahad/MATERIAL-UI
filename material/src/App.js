d0f0d5e7e2efab5d01798d3faf816d80b991c0d4
Sagar ghare <sagar.ghare@acc.ltd>
2023-10-04 12:11:00 +0530

    app.js edited

--- material/src/App.js
+++ material/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const design = (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<h1>Welcome to Home page</h1>} />
+        </Routes>
+      </Router>
+    </>
   );
+  return design;
 }
 
 export default App;
