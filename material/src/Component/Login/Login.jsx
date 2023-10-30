6a7941ac3f96e469cba1b7141e82ca488641871c
Sagar ghare <sagar.ghare@acc.ltd>
2023-10-30 18:15:22 +0530

    login page validation done

--- material/src/Component/Login/Login.jsx
+++ material/src/Component/Login/Login.jsx
@@ -7,14 +7,85 @@ import {
     Stack,
     TextField
 } from '@mui/material';
+import { useState } from 'react';
 import {
     Link,
     useNavigate
 } from 'react-router-dom';
+import * as yup from 'yup';
 
 // MAIN COMPONNENT OF LOGIN PAGE
 const Login = () => {
+    const [disabled, setDisabled] = useState(true);
+    const [input, setInput] = useState({
+        username: "",
+        password: ""
+    });
+    const [error, setError] = useState({
+        username: {
+            state: false,
+            message: ""
+        },
+        password: {
+            state: false,
+            message: ""
+        }
+    })
     const navigate = useNavigate();
+
+    // HANDLE INPUTS    
+    const handleInput = (e) => {
+        const input = e.target;
+        const key = input.name;
+        const value = input.value;
+
+        return setInput((oldData) => {
+            return {
+                ...oldData,
+                [key]: value
+            }
+        })
+    }
+
+    // YUP VALIDATION
+    const schema = yup.object().shape({
+        username: yup.string().required().email(),
+        password: yup.string().required().min(4).max(8)
+    });
+
+    const validateSubmit = async () => {
+        const isValid = await schema.isValid(input);
+        return setDisabled(!isValid);
+    }
+
+    const validateInput = async (e) => {
+        const key = e.target.name;
+        try {
+            await schema.validateAt(key, input);
+
+            return setError((oldData) => {
+                return {
+                    ...oldData,
+                    [key]: {
+                        state: false,
+                        message: ""
+                    }
+                }
+            })
+
+        } catch (error) {
+            const message = error.errors;
+            return setError((oldData) => {
+                return {
+                    ...oldData,
+                    [key]: {
+                        state: true,
+                        message: message[0]
+                    }
+                }
+            })
+        }
+    }
 
     //LOGIN FUNCTION CODING
     const login = (e) => {
@@ -27,8 +98,8 @@ const Login = () => {
         <>
             <Container>
                 <Grid container>
-                    <Grid item sm="6" xs="12">One</Grid>
-                    <Grid item sm="6" xs="12">
+                    <Grid item sm={6} xs={12}>One</Grid>
+                    <Grid item sm={6} xs={12}>
                         <h1>Login</h1>
                         <form onSubmit={login}>
                             <Stack direction={'column'} spacing={3}>
@@ -36,12 +107,24 @@ const Login = () => {
                                     name='username'
                                     label='Username'
                                     variant='outlined'
+                                    error={error.username.state}
+                                    helperText={error.username.message}
+                                    value={input.username}
+                                    onChange={handleInput}
+                                    onKeyDown={validateSubmit}
+                                    onInput={validateInput}
                                 />
                                 <TextField
                                     name='password'
                                     label='Password'
                                     type='password'
                                     variant='outlined'
+                                    error={error.password.state}
+                                    helperText={error.password.message}
+                                    value={input.password}
+                                    onChange={handleInput}
+                                    onKeyDown={validateSubmit}
+                                    onInput={validateInput}
                                 />
                                 <Stack
                                     direction={'row'}
@@ -62,6 +145,7 @@ const Login = () => {
                                             fontWeight: 'bold',
                                             fontSize: "18px"
                                         }}
+                                        disabled={disabled}
                                     >Login</Button>
                                 </Stack>
                                 <Stack
